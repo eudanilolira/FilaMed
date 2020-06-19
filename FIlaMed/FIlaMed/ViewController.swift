925cde4839c97fdbebf27be4ccbfea05522806f3
Danilo Lira <danilo.lira01@gmail.com>
2020-06-19 09:44:49 -0300

    Criação do projeto

---
+++ FIlaMed/FIlaMed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FIlaMed
+//
+//  Created by Danilo Araújo on 19/06/20.
+//  Copyright © 2020 FilaMed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
