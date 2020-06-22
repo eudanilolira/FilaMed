bbb8c1d6ee073d6bc014cd2d02fc223f4f65d18b
Pedro da Matta <pedrodamatta3@gmail.com>
2020-06-22 09:34:37 -0300

    Sets navigation controller

--- FIlaMed/FIlaMed/Controllers/AppointmentsViewController.swift
+++ FIlaMed/FIlaMed/Controllers/AppointmentsViewController.swift
@@ -13,6 +13,9 @@ class AppointmentsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.view = LoginView()
+        title = "Consultas"
         print("Hello Formiga!")
     }
 
