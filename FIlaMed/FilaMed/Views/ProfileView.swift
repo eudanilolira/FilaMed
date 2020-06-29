cd1e8a9bba1f6b5fc0ac8bdeb3ea83538659b2b8
Pedro da Matta <pmrm@cin.ufpe.br>
2020-06-29 12:39:42 -0300

    Adds profile picture without constraints

--- FIlaMed/FilaMed/Views/ProfileView.swift
+++ FIlaMed/FilaMed/Views/ProfileView.swift
@@ -9,13 +9,40 @@
 import UIKit
 
 class ProfileView: UIView {
+    let profilePicture = ProfilePicture()
     public init() {
         super.init(frame: .zero)
-        self.backgroundColor = .white
+        self.backgroundColor = GlobalStyle.BackgroundColor
+        self.addSubview(profilePicture)
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
+}
+
+class ProfilePicture: UIImageView {
+    public init() {
+        super.init(frame: CGRect(x: 200, y: 100, width: 60, height: 60))
+        self.setupLayout()
+//        self.setupConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setupLayout() {
+        let picture = UIImage(named: "profile_picture.png")
+        self.image = picture
+    }
+
+    func setupConstraints() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+//            self.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
 }
