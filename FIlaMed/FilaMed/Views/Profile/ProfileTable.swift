22ce861d735897ba0444e89f4e16d20fd8ad9983
Danilo Lira <danilo.lira01@gmail.com>
2020-06-29 13:37:17 -0300

    Creates profileTable, adds label and setup constraints in profile

---
+++ FIlaMed/FilaMed/Views/Profile/ProfileTable.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileTable.swift
+//  FilaMed
+//
+//  Created by Danilo Araújo on 29/06/20.
+//  Copyright © 2020 FilaMed. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTable: UITableView {
+    
+}
