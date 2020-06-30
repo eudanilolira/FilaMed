562e2c88de96d4aa2a817a094d5059b81239ea84
v1lton <wrs@cin.ufpe.br>
2020-06-30 14:47:39 -0300

    Conforms LineStackView to CodeView protocol

--- FIlaMed/FilaMed/Views/Line/LineStackView.swift
+++ FIlaMed/FilaMed/Views/Line/LineStackView.swift
@@ -8,21 +8,16 @@
 
 import UIKit
 
-class LineStackView: UIStackView {
+class LineStackView: UIStackView, CodeView {
 
     let doctorInformationView: DoctorInformationView = DoctorInformationView()
     let lineIndicatorView: LineIndicatorView = LineIndicatorView()
-    let outputButton: OutputButton = OutputButton()
+    //let outputButton: OutputButton = OutputButton()
 
     public init() {
         super.init(frame: .zero)
-        setupStyle()
-        //self.outputButton.heightAnchor.constraint(equalToConstant: 20).isActive = true
-        self.addArrangedSubview(doctorInformationView)
-        self.addArrangedSubview(lineIndicatorView)
-        self.addArrangedSubview(outputButton)
-        setupDoctorInformationView()
-        setupLineIndicatorView(upperView: doctorInformationView)
+        setupView()
+        //self.addArrangedSubview(outputButton)
     }
 
     required init(coder: NSCoder) {
@@ -30,26 +25,29 @@ class LineStackView: UIStackView {
     }
 
     func setupStyle() {
+
+    }
+
+    func buildViewHierarchy() {
+        self.addArrangedSubview(doctorInformationView)
+        self.addArrangedSubview(lineIndicatorView)
+    }
+
+    func setupContraints() {
+        self.doctorInformationView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.doctorInformationView.heightAnchor.constraint(equalToConstant: 216)
+        ])
+
+        self.lineIndicatorView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.lineIndicatorView.heightAnchor.constraint(equalToConstant: 216)
+        ])
+    }
+
+    func setupAdditionalConfiguration() {
         self.axis = .vertical
         self.spacing = 24
-        //self.distribution = .equalCentering
-        //self.alignment = .fill
     }
 
-    public func setupConstraints() {
-        self.translatesAutoresizingMaskIntoConstraints = false
-        self.leadingAnchor.constraint(equalTo: superview!.layoutMarginsGuide.leadingAnchor).isActive = true
-        self.trailingAnchor.constraint(equalTo: superview!.layoutMarginsGuide.trailingAnchor, constant: -8).isActive = true
-        self.topAnchor.constraint(equalTo: superview!.topAnchor).isActive = true
-        self.bottomAnchor.constraint(equalTo: superview!.bottomAnchor).isActive = true
-        self.widthAnchor.constraint(equalTo: superview!.widthAnchor).isActive = true
-    }
-
-    func setupDoctorInformationView() {
-        self.doctorInformationView.setupConstraints()
-    }
-
-    func setupLineIndicatorView(upperView: UIView) {
-        self.lineIndicatorView.setupConstraints(upperView)
-    }
 }
