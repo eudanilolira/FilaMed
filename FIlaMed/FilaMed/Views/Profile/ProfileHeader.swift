d7aa183a2acc5d9f010d38355c1ab75ae3d91127
Danilo Lira <danilo.lira01@gmail.com>
2020-06-30 12:30:17 -0300

    Fixes profiles views constraints

---
+++ FIlaMed/FilaMed/Views/Profile/ProfileHeader.swift
@@ -0,0 +1,54 @@
+//
+//  ProfileHeader.swift
+//  FilaMed
+//
+//  Created by Danilo Araújo on 30/06/20.
+//  Copyright © 2020 FilaMed. All rights reserved.
+//
+
+import UIKit
+
+class ProfilesHeader: UITableViewHeaderFooterView, CodeView {
+    let view = UIView()
+    let title = UILabel()
+
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        self.setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func buildViewHierarchy() {
+        contentView.addSubview(view)
+        contentView.addSubview(title)
+    }
+
+    func setupContraints() {
+        title.translatesAutoresizingMaskIntoConstraints = false
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            view.heightAnchor.constraint(equalToConstant: 30),
+            view.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            view.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+
+        NSLayoutConstraint.activate([
+            title.heightAnchor.constraint(equalTo: contentView.heightAnchor),
+            title.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            title.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            title.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+
+        ])
+    }
+
+    func setupAdditionalConfiguration() {
+        title.font = UIFont.boldSystemFont(ofSize: 17)
+        title.backgroundColor = GlobalStyle.BackgroundColor
+        view.backgroundColor =  GlobalStyle.BackgroundColor
+    }
+}
--- FIlaMed/FilaMed/Views/Profile/ProfileTable.swift
+++ FIlaMed/FilaMed/Views/Profile/ProfileTable.swift
@@ -8,75 +8,30 @@
 
 import UIKit
 
-class ProfileTable: UITableView {
+class ProfileTable: UITableView, CodeView {
+    func buildViewHierarchy() {
+    }
 
-    init() {
-        super.init(frame: .zero, style: UITableView.Style.plain)
-        self.setupStyle()
+    func setupContraints() {
+    }
+
+    func setupAdditionalConfiguration() {
         self.isScrollEnabled = false
         self.backgroundColor = .white
         self.layer.cornerRadius = 10
-    }
 
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    func setupConstraints() {
-        self.translatesAutoresizingMaskIntoConstraints = false
-        self.topAnchor.constraint(equalTo: superview!.subviews[0].bottomAnchor, constant: 150).isActive = true
-        self.heightAnchor.constraint(equalToConstant: 170).isActive = true
-        self.trailingAnchor.constraint(equalTo: superview!.layoutMarginsGuide.trailingAnchor).isActive = true
-        self.leadingAnchor.constraint(equalTo: superview!.layoutMarginsGuide.leadingAnchor).isActive = true
-    }
-
-    func setupStyle() {
         self.register(ProfilesHeader.self, forHeaderFooterViewReuseIdentifier: "profilesHeader")
         self.register(ProfileCell.self, forCellReuseIdentifier: "profileCell")
         self.backgroundColor = GlobalStyle.BackgroundColor
     }
-}
 
-class ProfilesHeader: UITableViewHeaderFooterView {
-    let view = UIView()
-    let title = UILabel()
+    init() {
+        super.init(frame: .zero, style: UITableView.Style.plain)
+        self.setupView()
 
-    override init(reuseIdentifier: String?) {
-        super.init(reuseIdentifier: reuseIdentifier)
-        self.setupStyle()
-        self.setupConstraints()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-
-    func setupStyle() {
-        contentView.addSubview(view)
-        contentView.addSubview(title)
-
-        title.font = UIFont.boldSystemFont(ofSize: 17)
-        title.backgroundColor = GlobalStyle.BackgroundColor
-        view.backgroundColor =  GlobalStyle.BackgroundColor
-    }
-
-    func setupConstraints() {
-        title.translatesAutoresizingMaskIntoConstraints = false
-        view.translatesAutoresizingMaskIntoConstraints = false
-
-        NSLayoutConstraint.activate([
-            view.heightAnchor.constraint(equalToConstant: 30),
-            view.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            view.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            view.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
-        ])
-
-        NSLayoutConstraint.activate([
-            title.heightAnchor.constraint(equalTo: contentView.heightAnchor),
-            title.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            title.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            title.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
-
-        ])
     }
 }
